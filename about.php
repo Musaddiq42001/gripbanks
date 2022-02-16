76bbcce3be9e61b7c6120b942a0ca618a00320fb
Musaddiq42001 <91488938+Musaddiq42001@users.noreply.github.com>
2022-02-16 20:30:22 +0530

    Add files via upload
    first commit

---
+++ about.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@800&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link rel="stylesheet" type="text/css" href="css/name.css">
+
+    <style type="text/css">
+    body{
+	   background-image:url("b3.jpg") ;
+	   position: center;
+     background-size:cover;
+	   height:100vh;
+    }
+    </style>
+  </head>
+
+  <body>
+    <?php
+      include 'name.php';
+    ?>
+    <br>
+    <br>
+    <h2> <strong class="cur">ABOUT US</strong></h2>
+    <p>
+      <h5>
+      Online banking allows a user to conduct financial transactions via the Internet. Online banking is also known as Internet banking or web banking. 
+      <br>
+      This system is seamlessly integrated with Core Banking System.
+      <br>
+      Online banking offers customers almost every service traditionally available through a local branch including deposits, transfers, and online bill payments.
+      <br>
+      It supports multi currency transactions and all types of delivery channels.
+  </h5>
+    </p>
+    <br>
+    <br>
+
+    <h2> <strong class="cur">CONTACT US</h2>
+    <hr color="black" >
+    <h4>Name : Musaddiq Shaikh</h4></strong>
+    <h3></h3></strong>
+    <hr color="black" >
+    <br>
+  </body>
+</html>
